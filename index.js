66723cf95c3fba4e7e5b66e90c388a47a2317b19
Julia Nikolaeva <jnikolaeva18@gmail.com>
2019-10-12 21:01:14 +0300

    Донастройка вебпака

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 
-// import "./index.css";
+import "./index.css";
 import Glide from '@glidejs/glide'
 
 new Glide('.glide').mount()
--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+        filename: '[name].[chunkhash].js'
     },
     module: {
         rules: [{
