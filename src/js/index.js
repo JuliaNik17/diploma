7e16090efa5071cf978a39308e005bf7f1d50404
Julia Nikolaeva <jnikolaeva18@gmail.com>
2019-10-24 10:08:50 +0300

    Исправления вёрстки

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,19 @@
 
 
 import "../css/pages/index.css";
+const box = document.querySelector('.card__text'),
+    text = box.innerHTML,
+    clone = document.createElement('div');
+
+clone.style.position = 'absolute';
+clone.style.visibility = 'hidden';
+clone.style.width = box.clientWidth + 'px';
+clone.innerHTML = text;
+document.body.appendChild(clone);
+
+var l = text.length - 1;
+for (; l >= 0 && clone.clientHeight > box.clientHeight; --l) {
+    clone.innerHTML = text.substring(0, l) + '...';
+}
+
+box.innerHTML = clone.innerHTML;
