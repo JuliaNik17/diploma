ecc9a1f814e623b437e75882b094c7b2999be30f
Julia Nikolaeva <jnikolaeva18@gmail.com>
2019-10-24 07:32:07 +0300

    БЭМ, исправлен адаптив страницы Аналатика

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,3 @@
 
 
-import "../css/index.css";
-
-//import Glide from '@glidejs/glide'
-
-// new Glide('.glide').mount()
-
-// var glide = new Glide('#intro', {
-//     // type: 'carousel',
-//     perView: 3,
-//     focusAt: 'center',
-//     breakpoints: {
-//         800: {
-//             perView: 2
-//         },
-//         480: {
-//             perView: 1
-//         }
-//     }
-// })
-
-// glide.mount()
+import "../css/pages/index.css";
